ee8f1373e5f1aa1ddb0907fadd451bbbeb1eb83c
leonatwork <noelabydas@gmail.com>
2020-08-12 18:57:13 +0530

    Added comments

--- server/index.js
+++ server/index.js
@@ -13,24 +13,30 @@ const {
   deleteTodo,
 } = require("../database");
 
+// Root
 app.get("/", (req, res) => res.send("Hello World"));
 
+// GET : get all todos
 app.get("/todos", async (req, res) => res.send(await findAllTodos()));
 
+// GET : get a todo
 app.get("/todos/:id", async (req, res) =>
   res.send(await findTodoById(req.params.id))
 );
 
+// POST : create a todo
 app.post("/todos", async (req, res) => {
   const todo = await addTodo(req.body);
   res.send(todo);
 });
 
+// PUT : update a todo
 app.put("/todos/:id", async (req, res) => {
   const todo = await updateTodo(req.params.id, req.body);
   res.send(todo);
 });
 
+// DELETE : delete a todo
 app.delete("/todos/:id", async (req, res) => {
   const todo = await deleteTodo(req.params.id);
   res.send(todo);
