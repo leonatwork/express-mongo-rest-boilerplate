9c803f39b2a96127594c7ffdcfb1157541ad183c
leonatwork <noelabydas@gmail.com>
2020-08-12 19:39:58 +0530

    Moved routes to different folder

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const app = require("./server");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 8000;
 
---
+++ routes/api/users.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  findAllUsers,
+  findUserById,
+  addUser,
+  updateUser,
+  deleteUser,
+} = require("../../database");
+
+// GET : get all users
+router.get("/", async (req, res) => res.send(await findAllUsers()));
+
+// GET : get a user
+router.get("/:id", async (req, res) => {
+  const user = await findUserById(req.params.id);
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(400).send("User NOT found");
+  }
+});
+
+// POST : create a user
+router.post("/", async (req, res) => {
+  const user = await addUser(req.body);
+  res.send(user);
+});
+
+// PUT : update a user
+router.put("/:id", async (req, res) => {
+  const user = await updateUser(req.params.id, req.body);
+  res.send(user);
+});
+
+// DELETE : delete a user
+router.delete("/:id", async (req, res) => {
+  const user = await deleteUser(req.params.id);
+  res.send(user);
+});
+
+module.exports = router;
